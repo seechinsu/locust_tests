038cdc4976ae08e05a1939972948880b23473606
seechinsu <seechinsu@gmail.com>
2019-01-13 16:38:47 -0500

    removed login so user does not lose session to continue requets

--- seraphLogin.py
+++ seraphLogin.py
@@ -1,53 +1,64 @@
 from locust import HttpLocust, TaskSet, task
 from bs4 import BeautifulSoup
 
-USER_CREDENTIALS = [
-    ("gatling1@gmail.com", "sechien123"),
-    ("gatling2@gmail.com", "sechien123"),
-    ("gatling3@gmail.com", "sechien123"),
-    ("gatling4@gmail.com", "sechien123"),
-    ("gatling5@gmail.com", "sechien123"),
-    ("gatling6@gmail.com", "sechien123"),
-    ("gatling7@gmail.com", "sechien123"),
-    ("gatling8@gmail.com", "sechien123"),
-    ("gatling9@gmail.com", "sechien123"),
-    ("gatling10@gmail.com", "sechien123")
-]
+USER_CREDENTIALS = []
+
+with open('creds.csv', 'r') as file:
+    lines = file.readlines()
+    USER_CREDENTIALS = [x.replace('\"', '').replace('\n','') for x in lines]
 
 class UserBehavior(TaskSet):
 
     def on_start(self):
         """ on_start is called when a Locust start before any task is scheduled """
         if len(USER_CREDENTIALS) > 0:
-            email, passw = USER_CREDENTIALS.pop()
-            self.login(email, passw)
+            email = USER_CREDENTIALS.pop()
+            # print(email)
+            self.login(email)
 
     def on_stop(self):
         """ on_stop is called when the TaskSet is stopping """
-        self.logout()
+        # self.logout()
 
-    def login(self, email, passw):
+    def login(self, email):
         response = self.client.get("/login")
         soup = BeautifulSoup(response.content, 'html.parser')
         csrfToken = soup.find(attrs={"name":"_csrf_token"})['value']
-        print(csrfToken)
-        self.client.post("/sessions", {"user[email]":email, "user[password]":passw, "_csrf_token":csrfToken})
+        # print(csrfToken)
+        self.client.post("/sessions", {"user[email]":email, "user[password]":"sechien123", "_csrf_token":csrfToken})
 
-    @task(2)
-    def logout(self):
-        self.client.get("/logout")
+    # @task(1)
+    # def logout(self):
+    #     self.client.get("/logout")
 
-    @task(2)
+    @task(5)
     def index(self):
         self.client.get("/")
 
-    @task(3)
+    @task(4)
     def profile(self):
         self.client.get("/profiles")
 
-    @task(2)
+    @task(4)
     def project(self):
         self.client.get("/projects")
+
+    @task(3)
+    def newProject(self):
+        response = self.client.get("/projects/new")
+        # print(response)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        csrfToken = soup.find(attrs={"name":"_csrf_token"})['value']
+        self.client.post("/projects", {"project[title]":"test", "project[content]":"test", "project[github_link]":"https://github.com/seechinsu/elixir-phoenix", "_csrf_token":csrfToken})
+
+    @task(3)
+    def newProfile(self):
+        response = self.client.get("/profiles/new")
+        # print(response)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        csrfToken = soup.find(attrs={"name":"_csrf_token"})['value']
+        self.client.post("/profiles", {"profile[title]":"Locust", "profile[team]":"Elixir", "profile[pic_link]":"https://www.geek.com/wp-content/uploads/2017/08/squidward-anime-625x352.jpg", "profile[bio]":"Locust", "profile[github_link]":"https://github.com/seechinsu", "profile[linkedin_link]": "https://www.linkedin.com/in/se-chien-nick-hsu-b5723a37/", "profile[twitter_link]": "https://twitter.com/seechinsu", "_csrf_token":csrfToken})
+
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
